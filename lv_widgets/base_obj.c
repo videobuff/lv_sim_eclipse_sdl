7e345ee100a785706901bf8713fd58d057646428
videobuff <fhaschott@gmail.com>
2021-02-02 13:51:02 +0100

    Update base_obj.c

--- lv_widgets/base_obj.c
+++ lv_widgets/base_obj.c
@@ -8,9 +8,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "base_obj.h"
+#include "../lvgl/lvgl.h"
 
 void base_obj(void)
 {
 	printf("base_obj\n");
+
+	lv_obj_t * base_obj = lv_obj_create(lv_scr_act(),NULL);
 	fflush(NULL);
 }
