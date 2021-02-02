fddacf436b121c6437b8432c7aafad23fa75e237
videobuff <fhaschott@gmail.com>
2021-02-02 14:12:37 +0100

    Update base_obj.c

--- lv_widgets/base_obj.c
+++ lv_widgets/base_obj.c
@@ -15,5 +15,15 @@ void base_obj(void)
 	printf("base_obj\n");
 
 	lv_obj_t * base_obj = lv_obj_create(lv_scr_act(),NULL);
+	lv_obj_set_drag(base_obj, true);
+	lv_obj_set_size(base_obj, 200, 200);
+	lv_obj_set_pos(base_obj,200,200);
+
+	lv_obj_t * btn = lv_btn_create(lv_scr_act(),NULL);
+	lv_obj_set_drag(btn, true);
+	lv_obj_set_size(btn, 100, 100);
+	lv_obj_set_pos(btn,100,100);
+
+
 	fflush(NULL);
 }
